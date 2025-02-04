b59d29e731a5fbd0ba7a7f52888013c6902a35ce
Pe-drou <phsr300408@gmail.com>
2025-02-04 12:12:59 -0300

    chrome

---
+++ exemplo1-chrome/script.js
@@ -0,0 +1,4 @@
+console.log("Nao_liga_pro_alerta");
+// console.log faz mensagens aparecer no console
+alert("Seu computer está com virús!")
+// alert faz mensagens aparecer na página dentro de uma caixa
