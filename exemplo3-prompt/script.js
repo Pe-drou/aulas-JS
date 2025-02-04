080efab050cecf1b9ee8a3a8a83ab950e15e3461
Pe-drou <phsr300408@gmail.com>
2025-02-04 12:13:14 -0300

    prompt

---
+++ exemplo3-prompt/script.js
@@ -0,0 +1,4 @@
+let nome = prompt("digite o seu nome")
+let idade = prompt("digite a sua idade")
+
+document.getElementById("result").innerText = "seu nome é " + nome + " e sua idade é " + idade + " anos."
