571c942ced5c982f3ca072da3f2140da288b9423
Pe-drou <phsr300408@gmail.com>
2025-02-04 12:13:07 -0300

    variáveis

---
+++ exemplo2-variavel/script.js
@@ -0,0 +1,6 @@
+let nome = ('Baiano')
+let bairro = ('Baianal')
+console.log(nome)
+
+document.getElementById("resultado").innerText = "Seu nome é: " + nome 
++ ", Você mora em: " + bairro + ". Tenha um bom dia"
